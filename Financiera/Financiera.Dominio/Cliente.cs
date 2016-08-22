7b5797ab5e583b5a61a3a9bf48e52f3c603403ed
Usuario <Usuario@A_DOCENTE>
2016-08-22 16:14:26 -0500

    version 1

---
+++ Financiera/Financiera.Dominio/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financiera.Dominio
+{
+    public class Cliente
+    {
+        public int CodigoCliente { get; set; }
+        public string NombreCliente { get; set; }
+        public byte TipoCliente { get; set; }
+
+        public Cliente DarAlta(string as_nombre, byte aby_tipo)
+        {
+            return new Cliente() { NombreCliente = as_nombre, TipoCliente = aby_tipo  };
+        }
+
+        public void CambiarNombre(string as_nuevo_nombre)
+        {
+
+            NombreCliente = as_nuevo_nombre;
+        }
+    }
+}
---
+++ Financiera/Financiera.Dominio/CuentaCorriente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financiera.Dominio
+{
+    public class CuentaCorriente
+    {
+        public int NumeroCuenta { get; set; }
+        public string CodigoCuenta { get; set; }
+        public string CodigoCliente { get; set; }
+        public decimal SaldoCuenta { get; set; }
+        public DateTime FechaApertura { get; set; }
+        public byte EstadoCuenta { get; set; }
+
+        public virtual Cliente Propietario { get; set; }
+
+        public CuentaCorriente Aperturar()
+        {
+            return null;
+        }
+        public void Cancelar()
+        {
+        }
+
+        public void Bloquear()
+        {
+        }
+
+        public void Desbloquear()
+        { }
+    }
+}
