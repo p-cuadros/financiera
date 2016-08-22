c4b6d0b2359e483fb7ae0d5006af4212416031ae
Usuario <Usuario@A_DOCENTE>
2016-08-22 16:36:23 -0500

    comentarios

--- Financiera/Financiera.Dominio/CuentaCorriente.cs
+++ Financiera/Financiera.Dominio/CuentaCorriente.cs
@@ -6,31 +6,52 @@ using System.Threading.Tasks;
 
 namespace Financiera.Dominio
 {
+    /// <summary>
+    /// Clase de Cominio que representa las Cuentas Corrientes
+    /// </summary>
     public class CuentaCorriente
     {
+        /// <summary>
+        /// Propiedad Unica de cada Cuenta
+        /// </summary>
         public int NumeroCuenta { get; set; }
         public string CodigoCuenta { get; set; }
-        public string CodigoCliente { get; set; }
+        public int CodigoCliente { get; set; }
         public decimal SaldoCuenta { get; set; }
         public DateTime FechaApertura { get; set; }
         public byte EstadoCuenta { get; set; }
-
         public virtual Cliente Propietario { get; set; }
 
-        public CuentaCorriente Aperturar()
+        /// <summary>
+        /// Metodo que devuelve una cuenta corriente nueva
+        /// </summary>
+        /// <param name="as_cod_cuenta">Codigo de la Cuenta</param>
+        /// <param name="ao_cliente">Cliente de Cuenta</param>
+        /// <returns>Nueva Cuenta corriente</returns>
+        public static CuentaCorriente Aperturar(string as_cod_cuenta, Cliente ao_cliente)
         {
-            return null;
+            return new CuentaCorriente()
+            {
+                CodigoCuenta = as_cod_cuenta,
+                CodigoCliente = ao_cliente.CodigoCliente,
+                SaldoCuenta = 0,
+                FechaApertura = DateTime.Now,
+                EstadoCuenta = 1,
+                Propietario = ao_cliente
+            };
         }
         public void Cancelar()
         {
+            SaldoCuenta = 0;
+            EstadoCuenta = 4;
         }
-
         public void Bloquear()
         {
             EstadoCuenta = 2;
         }
-
         public void Desbloquear()
-        { }
+        {
+            EstadoCuenta = 3;
+        }
     }
 }
