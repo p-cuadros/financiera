4c52d12c9fcdacb5d100b1126620bd34cfe57e2f
Usuario <Usuario@A_DOCENTE>
2016-08-31 16:52:27 -0500

    pruebas unitarias

---
+++ Financiera/Financiera.Pruebas/ClientePruebasUnitarias.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Financiera.Dominio;
+
+namespace Financiera.Pruebas
+{
+    [TestClass]
+    public class ClientePruebasUnitarias
+    {
+        [TestMethod]
+        public void DarAltaSatisfactorio()
+        {
+            var cliente = Cliente.DarAlta("Hector Maquera",1);
+            Assert.IsNotNull(cliente);
+        }
+    }
+}
